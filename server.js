e64ebb5a934fc42cd62ce030e80c52518ecf5a2d
mahmodam <91775238+mahmodam@users.noreply.github.com>
2023-01-06 17:08:00 +0200

    adding routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const users = require("./routes/api/users");
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,6 +14,11 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
